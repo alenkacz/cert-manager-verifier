3f446f2267ecab200c7dc79b7bd38988ea67bd0d
Alena Varkockova <varkockova.a@gmail.com>
2020-12-31 10:25:42 +0100

    E2E test setup using kuttl

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ test:
 .PHONY: build
 build:
 	go build -race -o ./bin/cm-verifier cmd/ctl/main.go
+
+.PHONY: e2e-test
+e2e-test:
+	./hack/run-e2e-tests.sh
---
+++ hack/run-e2e-tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+KUTTL_VERSION="0.7.2"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    curl -L https://github.com/kudobuilder/kuttl/releases/download/v${KUTTL_VERSION}/kubectl-kuttl_${KUTTL_VERSION}_darwin_x86_64 --output kuttl
+elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
+    curl -L https://github.com/kudobuilder/kudo/releases/download/v${KUTTL_VERSION}/kubectl-kuttl_${KUTTL_VERSION}_linux_x86_64 --output kuttl
+fi
+
+chmod +x kuttl
+
+./kuttl test --config test/e2e/e2e-test.yaml

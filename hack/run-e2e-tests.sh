5d12bbbac52239f896680dbda389b04d22920e05
Alena Varkockova <varkockova.a@gmail.com>
2021-01-01 15:06:12 +0100

    Fix linux command

--- hack/run-e2e-tests.sh
+++ hack/run-e2e-tests.sh
@@ -10,7 +10,7 @@ KUTTL_VERSION="0.7.2"
 if [[ "$(uname)" == "Darwin" ]]; then
     curl -L https://github.com/kudobuilder/kuttl/releases/download/v${KUTTL_VERSION}/kubectl-kuttl_${KUTTL_VERSION}_darwin_x86_64 --output kuttl
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-    curl -L https://github.com/kudobuilder/kudo/releases/download/v${KUTTL_VERSION}/kubectl-kuttl_${KUTTL_VERSION}_linux_x86_64 --output kuttl
+    curl -L https://github.com/kudobuilder/kuttl/releases/download/v${KUTTL_VERSION}/kubectl-kuttl_${KUTTL_VERSION}_linux_x86_64 --output kuttl
 fi
 
 chmod +x kuttl
